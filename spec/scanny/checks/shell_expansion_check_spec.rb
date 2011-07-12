b2f0c4fee6efcf5669e4eb230b9ca148c5aefd8b
David Majda <david@majda.cz>
2011-07-12 16:22:23 +0200

    Use RSpec's fluent matcher interface for ShellExpansionCheck specs

--- spec/scanny/checks/shell_expansion_check_spec.rb
+++ spec/scanny/checks/shell_expansion_check_spec.rb
@@ -1,43 +1,23 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe Scanny::Checks::ShellExpansionCheck do
+
   before(:each) do
     @scanny = Scanny::Core::Runner.new(Scanny::Checks::ShellExpansionCheck.new)
   end
 
   it "does not report regular method calls" do
-    content = <<-EOT
-      foo
-    EOT
-
-    @scanny.check_content(content)
-    issues = @scanny.issues
-
-    issues.should be_empty
+    @scanny.should parse("foo").without_issues
   end
 
   it "reports \"exec\" calls" do
-    content = <<-EOT
-      exec
-    EOT
-
-    @scanny.check_content(content)
-    issues = @scanny.issues
-
-    issues.size.should == 1
-    issues[0].to_s.should == "[high] dummy-file.rb:2 - The \"exec\" method can pass the executed command through shell exapnsion."
+    @scanny.should parse("exec").with_issue(:high,
+      "The \"exec\" method can pass the executed command through shell exapnsion.")
   end
 
   it "reports \"system\" calls" do
-    content = <<-EOT
-      system
-    EOT
-
-    @scanny.check_content(content)
-    issues = @scanny.issues
-
-    issues.size.should == 1
-    issues[0].to_s.should == "[high] dummy-file.rb:2 - The \"system\" method can pass the executed command through shell exapnsion."
+    @scanny.should parse("system").with_issue(:high,
+      "The \"system\" method can pass the executed command through shell exapnsion.")
   end
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,27 @@
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + "/../lib"))
 require 'scanny'
 require 'rspec'
+
+RSpec::Matchers.define :parse do |content|
+  chain :without_issues do
+    @impact = nil
+    @message = nil
+  end
+
+  chain :with_issue do |impact, message|
+    @impact, @message = impact, message
+  end
+
+  match do |scanny|
+    scanny.check_content(content)
+    issues = scanny.issues
+
+    if @impact && @message
+      issues.size.should == 1
+      issues[0].impact.should == @impact
+      issues[0].message.should == @message
+    else
+      issues.should be_empty
+    end
+  end
+end
