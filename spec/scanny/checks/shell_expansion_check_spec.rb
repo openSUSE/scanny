8a29141688f30f454061e518bc9908f6e8969ebe
David Majda <david@majda.cz>
2011-07-13 10:27:17 +0200

    Wrap specs into "module" statement to avoid qualifications inside them

--- spec/scanny/checks/shell_expansion_check_spec.rb
+++ spec/scanny/checks/shell_expansion_check_spec.rb
@@ -1,38 +1,40 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
-describe Scanny::Checks::ShellExpansionCheck do
-  before :each do
-    @scanny = Scanny::Runner.new(Scanny::Checks::ShellExpansionCheck.new)
-  end
-
-  it "does not report regular method calls" do
-    @scanny.should parse('foo').without_issues
-  end
-
-  describe "reported methods" do
-    it "reports \"exec\" calls" do
-      @scanny.should parse('exec "ls -l"').with_issue(:high,
-        "The \"exec\" method can pass the executed command through shell exapnsion.")
+module Scanny::Checks
+  describe ShellExpansionCheck do
+    before :each do
+      @scanny = Scanny::Runner.new(ShellExpansionCheck.new)
     end
 
-    it "reports \"system\" calls" do
-      @scanny.should parse('system "ls -l"').with_issue(:high,
-        "The \"system\" method can pass the executed command through shell exapnsion.")
-    end
-  end
-
-  describe "argument counts" do
-    it "does not report calls with no arguments" do
-      @scanny.should parse('exec').without_issues
+    it "does not report regular method calls" do
+      @scanny.should parse('foo').without_issues
     end
 
-    it "reports calls with one argument" do
-      @scanny.should parse('exec "ls -l"').with_issue(:high,
-        "The \"exec\" method can pass the executed command through shell exapnsion.")
+    describe "reported methods" do
+      it "reports \"exec\" calls" do
+        @scanny.should parse('exec "ls -l"').with_issue(:high,
+          "The \"exec\" method can pass the executed command through shell exapnsion.")
+      end
+
+      it "reports \"system\" calls" do
+        @scanny.should parse('system "ls -l"').with_issue(:high,
+          "The \"system\" method can pass the executed command through shell exapnsion.")
+      end
     end
 
-    it "does not report calls with multiple arguments" do
-      @scanny.should parse('exec "ls", "-l"').without_issues
+    describe "argument counts" do
+      it "does not report calls with no arguments" do
+        @scanny.should parse('exec').without_issues
+      end
+
+      it "reports calls with one argument" do
+        @scanny.should parse('exec "ls -l"').with_issue(:high,
+          "The \"exec\" method can pass the executed command through shell exapnsion.")
+      end
+
+      it "does not report calls with multiple arguments" do
+        @scanny.should parse('exec "ls", "-l"').without_issues
+      end
     end
   end
 end
--- spec/scanny/runner_spec.rb
+++ spec/scanny/runner_spec.rb
@@ -1,25 +1,26 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe Scanny::Runner do
+module Scanny
+  describe Runner do
+    describe "given a custom config file" do
+      before do
+        @runner = Runner.new
+        @runner.config= File.expand_path(File.dirname(__FILE__) + '/scanny.yml')
+      end
 
-  describe "given a custom config file" do
-    before do
-      @runner = Scanny::Runner.new
-      @runner.config= File.expand_path(File.dirname(__FILE__) + '/scanny.yml')
-    end
+      it "uses check from it" do
+        # @runner.check_file(File.expand_path(File.dirname(__FILE__) + '/../fixtures/test_class.rb'))
+        content = <<-RUBY
+          class TestClass
 
-    it "uses check from it" do
-      # @runner.check_file(File.expand_path(File.dirname(__FILE__) + '/../fixtures/test_class.rb'))
-      content = <<-RUBY
-        class TestClass
+            def METHOD
 
-          def METHOD
-
+            end
           end
-        end
-      RUBY
-      @runner.check_content(content)
-      @runner.issues.should be_empty
+        RUBY
+        @runner.check_content(content)
+        @runner.issues.should be_empty
+      end
     end
   end
 end
