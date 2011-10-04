89f61db04b78ad4d906e0f719dc5d9cd33c97f43
Flavio Castelli <fcastelli@suse.com>
2011-10-04 21:36:36 +0200

    Define pattern method inside of Check
    Make easier to understand how to write custom checks.

--- lib/scanny/checks/check.rb
+++ lib/scanny/checks/check.rb
@@ -11,6 +11,12 @@ module Scanny
         @issues
       end
 
+      # @return [String] pattern used to find relevant nodes. It must respect Machete's syntax.
+      def pattern
+        raise "The Check class requires its childrens to provide an "\
+              "implementation of the 'pattern' method."
+      end
+
       def issue(impact, message, options = {})
         @issues << Issue.new(@file, @line, impact, message, options[:cwe])
       end
