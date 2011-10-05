58c2a2baaac4a5b04d930c30a0d7721028532ddd
David Majda <dmajda@suse.cz>
2011-10-05 15:16:46 +0200

    Implement BeforeFiltersCheck
    Part of a fix for GH-19.

---
+++ lib/scanny/checks/before_filters_check.rb
@@ -0,0 +1,32 @@
+module Scanny
+  module Checks
+    # Checks for use of the "before_filter" method with :login_required or
+    # :admin_required filters.
+    class BeforeFiltersCheck < Check
+      def pattern
+        <<-EOT
+          SendWithArguments<
+            receiver  = Self,
+            arguments = ActualArguments<
+              array = [
+                any*,
+                SymbolLiteral<value = :login_required | :admin_required>,
+                any*
+              ]
+            >
+          >
+        EOT
+      end
+
+      def check(node)
+        filter_node = node.arguments.array.find do |argument|
+          argument.is_a?(Rubinius::AST::SymbolLiteral) &&
+            [:login_required, :admin_required].include?(argument.value)
+        end
+
+        issue :info,
+          "The \"before_filter\" method with :#{filter_node.value} filter is used."
+      end
+    end
+  end
+end
---
+++ spec/scanny/checks/before_filters_check_spec.rb
@@ -0,0 +1,43 @@
+require "spec_helper"
+
+module Scanny::Checks
+  describe BeforeFiltersCheck do
+    before :each do
+      @runner = Scanny::Runner.new(BeforeFiltersCheck.new)
+      @login_required_issue = issue(:info,
+        "The \"before_filter\" method with :login_required filter is used.",
+        nil)
+      @admin_required_issue = issue(:info,
+        "The \"before_filter\" method with :admin_required filter is used.",
+        nil)
+    end
+
+    it "reports \"before_filter\" with :login_required filter correctly" do
+      @runner.should check(
+        'before_filter :login_required'
+      ).with_issue(@login_required_issue)
+      @runner.should check(
+        'self.before_filter :login_required'
+      ).with_issue(@login_required_issue)
+      @runner.should check('foo.before_filter :login_required').without_issues
+      @runner.should check(
+        'before_filter :some_filter, :login_required, :another_filter'
+      ).with_issue(@login_required_issue)
+      @runner.should check('before_filter :some_filter').without_issues
+    end
+
+    it "reports \"before_filter\" with :admin_required filter correctly" do
+      @runner.should check(
+        'before_filter :admin_required'
+      ).with_issue(@admin_required_issue)
+      @runner.should check(
+        'self.before_filter :admin_required'
+      ).with_issue(@admin_required_issue)
+      @runner.should check('foo.before_filter :admin_required').without_issues
+      @runner.should check(
+        'before_filter :some_filter, :admin_required, :another_filter'
+      ).with_issue(@admin_required_issue)
+      @runner.should check('before_filter :some_filter').without_issues
+    end
+  end
+end
