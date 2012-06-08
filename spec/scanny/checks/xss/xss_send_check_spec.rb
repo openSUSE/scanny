80cd192f0ed2922c692da693d0239bbcefc3c7f5
Piotr Niełacny <piotr.nielacny@gmail.com>
2012-06-08 13:21:56 +0200

    Update warning messages in specs

--- spec/scanny/checks/xss/xss_send_check_spec.rb
+++ spec/scanny/checks/xss/xss_send_check_spec.rb
@@ -4,7 +4,8 @@ module Scanny::Checks
   describe XssSendCheck do
     before :each do
       @runner = Scanny::Runner.new(XssSendCheck.new)
-      @issue = Scanny::Issue.new("scanned_file.rb", 1, :high, "XSS issue", 79)
+      @warning_message = XssSendCheck.new.send(:warning_message)
+      @issue = Scanny::Issue.new("scanned_file.rb", 1, :high, @warning_message, 79)
     end
 
     it "reports \"send_file :disposition => 'inline'\" correctly" do
