a60bcab7034c94589bc7759d8f5cdb26a3c917a6
Piotr Niełacny <piotr.nielacny@gmail.com>
2012-06-18 13:36:08 +0200

    Add checks for find, execute and paginate methods
    Add check for find method with :conditions argument

---
+++ lib/scanny/checks/sql_injection/find_method_check.rb
@@ -0,0 +1,80 @@
+require_relative "sql_check"
+
+module Scanny
+  module Checks
+    module Sql
+      # Check for methods executing external params on
+      # database engine
+      class FindMethodCheck < SqlCheck
+        def pattern
+          [
+            pattern_find_by_sql_and_execute_on_models,
+            pattern_find_by_with_params,
+            pattern_find_by_with_conditions
+          ].join("|")
+        end
+
+        def check(node)
+          issue :low, warning_message, :cwe => 89
+        end
+
+        private
+
+        # User.find_by_sql
+        def pattern_find_by_sql_and_execute_on_models
+          <<-EOT
+            Send<
+              name = :execute | :find_by_sql | :paginate,
+              receiver = ConstantAccess
+            >
+          EOT
+        end
+
+        # find_by_id(params[:search])
+        def pattern_find_by_with_params
+          <<-EOT
+            SendWithArguments<
+              arguments = ActualArguments<
+                array = [
+                  any*,
+                  SendWithArguments<
+                    name = :[],
+                    receiver = Send<
+                      name = :params
+                    >
+                  >,
+                  any*
+                ]
+              >,
+              name ^= :find_by
+            >
+          EOT
+        end
+
+        # find(:first, :conditions => "string")
+        def pattern_find_by_with_conditions
+          <<-EOT
+            SendWithArguments<
+              arguments = ActualArguments<
+                array = [
+                  any*,
+                  HashLiteral<
+                    array = [
+                      any*,
+                      SymbolLiteral<
+                        value = :conditions
+                      >,
+                      any*
+                    ]
+                  >,
+                  any*
+                ]
+              >,
+              name = :find
+            >
+          EOT
+        end
+      end
+    end
+  end
+end
