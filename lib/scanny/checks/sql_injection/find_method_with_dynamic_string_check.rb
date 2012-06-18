4a0b4c2934d1096afb2b88178e75a8a3697aec0b
Piotr Niełacny <piotr.nielacny@gmail.com>
2012-06-18 13:38:53 +0200

    Add check for method find with dynamic string as argument

---
+++ lib/scanny/checks/sql_injection/find_method_with_dynamic_string_check.rb
@@ -0,0 +1,44 @@
+require_relative "sql_check"
+
+module Scanny
+  module Checks
+    module Sql
+      # Check for methods executing external params on
+      # database engine with dynamic string
+      class FindMethodWithDynamicStringCheck < SqlCheck
+        def pattern
+          pattern_find_by_with_conditions_dynamic_string
+        end
+
+        def check(node)
+          issue :medium, warning_message, :cwe => 89
+        end
+
+        private
+
+        # find(:first, :conditions => "#{string}")
+        def pattern_find_by_with_conditions_dynamic_string
+          <<-EOT
+            SendWithArguments<
+              arguments = ActualArguments<
+                array = [
+                  any*,
+                  HashLiteral<
+                    array = [
+                      any*,
+                      SymbolLiteral<value = :conditions>,
+                      DynamicString,
+                      any*
+                    ]
+                  >,
+                  any*
+                ]
+              >,
+              name = :find
+            >
+          EOT
+        end
+      end
+    end
+  end
+end
