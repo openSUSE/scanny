c33f1041d38c6d08a92fd8208ccf4275c9749cb1
Piotr Niełacny <piotr.nielacny@gmail.com>
2012-06-18 13:40:12 +0200

    Add check for sanitize_sql method

---
+++ lib/scanny/checks/sql_injection/sanitize_sql_check.rb
@@ -0,0 +1,24 @@
+require_relative "sql_check"
+
+module Scanny
+  module Checks
+    module Sql
+      # Check for use of the "sanitize_sql" method
+      class SanitizeCheck < SqlCheck
+        def pattern
+          pattern_sanitize_sql
+        end
+
+        def check(node)
+          issue :info, warning_message, :cwe => 89
+        end
+
+        private
+
+        def pattern_sanitize_sql
+          "Send<name = :sanitize_sql>"
+        end
+      end
+    end
+  end
+end
