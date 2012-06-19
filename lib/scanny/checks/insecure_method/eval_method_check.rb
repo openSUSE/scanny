44d047465106e237b465885c493361bf001ab828
Piotr Niełacny <piotr.nielacny@gmail.com>
2012-06-20 00:24:42 +0200

    Add check for eval method

---
+++ lib/scanny/checks/insecure_method/eval_method_check.rb
@@ -0,0 +1,28 @@
+module Scanny
+  module Checks
+    module InsecureMethod
+      class EvalMethodCheck < Check
+        def pattern
+          pattern_eval_call
+        end
+
+        def check(node)
+          issue :high, warning_message, :cwe => 95
+        end
+
+        private
+
+        def warning_message
+          "Execute eval method can lead the ruby interpreter to run dangerous code"
+        end
+
+        def pattern_eval_call
+          <<-EOT
+            SendWithArguments | Send
+              <name = :eval>
+          EOT
+        end
+      end
+    end
+  end
+end
