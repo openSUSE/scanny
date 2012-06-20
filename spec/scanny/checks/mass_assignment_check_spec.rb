676304eac656d8cc7e49049f2a4fe80f8315684e
Piotr Niełacny <piotr.nielacny@gmail.com>
2012-06-20 12:05:10 +0200

    Add mass-assignment check

---
+++ spec/scanny/checks/mass_assignment_check_spec.rb
@@ -0,0 +1,25 @@
+require "spec_helper"
+
+module Scanny::Checks
+  describe MassAssignmentCheck do
+    before do
+      @runner = Scanny::Runner.new(MassAssignmentCheck.new)
+      @message =  "Create objects without defense against mass assignment" +
+                  "can cause dangerous errors in the database"
+      @issue = issue(:high, @message, 642)
+    end
+
+    it "reports \"User.new(params[:user])\" correctly" do
+      @runner.should check("User.new(params[:user])").with_issue(@issue)
+    end
+
+    it "reports \"User.create(params[:user])\" correctly" do
+      @runner.should check("User.create(params[:user])").with_issue(@issue)
+    end
+
+    it "reports \"@user.update_attributes(params[:user])\" correctly" do
+      @runner.should check("@user.update_attributes(params[:user])").with_issue(@issue)
+    end
+
+  end
+end
