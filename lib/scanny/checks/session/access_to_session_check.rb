a12a4fde795895a8254b3c3311cfe84605bc5121
Piotr Niełacny <piotr.nielacny@gmail.com>
2012-06-22 16:53:05 +0200

    Add check for access to session

---
+++ lib/scanny/checks/session/access_to_session_check.rb
@@ -0,0 +1,43 @@
+module Scanny
+  module Checks
+    module Session
+      class AccessToSessionCheck < Check
+        def pattern
+          [
+              pattern_session_access,
+              pattern_session_assignment
+          ].join("|")
+        end
+
+        def check(node)
+          issue :info, warning_message, :cwe => 0
+        end
+
+        private
+
+        def warning_message
+          "Referring to a session in the wrong way" +
+              "can lead to errors that reduce security level"
+        end
+
+        def pattern_session_access
+          <<-EOT
+          SendWithArguments<
+            name = :[],
+            receiver = Send<name = :session | :cookie>
+          >
+          EOT
+        end
+
+        def pattern_session_assignment
+          <<-EOT
+          ElementAssignment<
+            name = :[]=,
+            receiver = Send<name = :session | :cookie>
+          >
+          EOT
+        end
+      end
+    end
+  end
+end
---
+++ spec/scanny/checks/session/access_to_session_check_spec.rb
@@ -0,0 +1,29 @@
+require "spec_helper"
+
+module Scanny::Checks::Session
+  describe AccessToSessionCheck do
+    before do
+      @runner = Scanny::Runner.new(AccessToSessionCheck.new)
+      @message =  "Referring to a session in the wrong way" +
+                  "can lead to errors that reduce security level"
+      @issue = issue(:info, @message, 0)
+    end
+
+    it "reports \"session[:password]\" correctly" do
+      @runner.should check("session[:password]").with_issue(@issue)
+    end
+
+    it "reports \"cookie[:password]\" correctly" do
+      @runner.should check("cookie[:password]").with_issue(@issue)
+    end
+
+    it "reports \"session[:password] = params[:input]\" correctly" do
+      @runner.should check("session[:password] = nil").with_issue(@issue)
+    end
+
+    it "reports \"cookie[:password] = params[:input]\" correctly" do
+      @runner.should check("cookie[:password] = nil").with_issue(@issue)
+    end
+  end
+end
+
