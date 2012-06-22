43eaae19eee399c76b8df43e72b6fa38df6a2b04
Piotr Niełacny <piotr.nielacny@gmail.com>
2012-06-22 13:50:27 +0200

    Add verify peer check

---
+++ spec/scanny/checks/ssl/verify_peer_check_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+module Scanny::Checks::SSL
+  describe VerifyPeerCheck do
+    before do
+      @runner = Scanny::Runner.new(VerifyPeerCheck.new)
+      @message =  "Change the value of of VERIFY_PEER" +
+                  "can lead to faulty accepted certificate"
+      @issue = issue(:info, @message, 0)
+    end
+
+    it "reports \"OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE\" correctly" do
+      @runner.should  check("OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE").
+                      with_issue(@issue)
+    end
+  end
+end
