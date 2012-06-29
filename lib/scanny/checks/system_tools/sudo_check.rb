a57c3a9043e991ca389b0a380b9c5b65495081c8
Piotr Niełacny <piotr.nielacny@gmail.com>
2012-06-29 12:22:05 +0200

    Add check for 'sudo' command

---
+++ lib/scanny/checks/system_tools/sudo_check.rb
@@ -0,0 +1,24 @@
+module Scanny
+  module Checks
+    module SystemTools
+      class SudoCheck < Check
+        include Scanny::Checks::Helpers
+
+        def pattern
+          build_pattern_exec_command('sudo')
+        end
+
+        def check(node)
+          issue :info, warning_message, :cwe => 0
+        end
+
+        private
+
+        def warning_message
+          "Using sudo can lead to the execution" +
+          "of programs on root administrator rights"
+        end
+      end
+    end
+  end
+end
