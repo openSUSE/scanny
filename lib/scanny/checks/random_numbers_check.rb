8782e7ad513b1cdfb13a5f51b467d85bd0eefcd5
Piotr Niełacny <piotr.nielacny@gmail.com>
2012-07-02 13:40:35 +0200

    Add check for urandom and seed

--- lib/scanny/checks/random_numbers_check.rb
+++ lib/scanny/checks/random_numbers_check.rb
@@ -3,16 +3,47 @@ module Scanny
     # Checks for indication that a low-entropy random number generator is used.
     class RandomNumbersCheck < Check
       def pattern
-        <<-EOT
-          Send<receiver = Self | ConstantAccess<name = :Kernel>, name = :rand | :srand>
-          |
-          SendWithArguments<receiver = Self | ConstantAccess<name = :Kernel>, name = :rand | :srand>
-        EOT
+        [
+          pattern_rand,
+          pattern_seed,
+          pattern_urandom
+        ].join("|")
       end
 
       def check(node)
-        issue :medium, "The \"#{node.name}\" method indicates using low-entropy random number generator.",
-              :cwe => 331
+        issue :medium, warning_message, :cwe => 331
+      end
+
+      private
+
+      def warning_message
+        "This action indicates using low-entropy random number generator"
+      end
+
+      def pattern_rand
+        <<-EOT
+          Send<
+            receiver = Self | ConstantAccess<name = :Kernel>,
+            name = :rand | :srand
+          >
+          |
+          SendWithArguments<
+            receiver = Self | ConstantAccess<name = :Kernel>,
+            name = :rand | :srand
+          >
+        EOT
+      end
+
+      def pattern_seed
+        <<-EOT
+          Send<name = :seed>
+          |
+          SendWithArguments<name = :seed>
+        EOT
+      end
+
+      def pattern_urandom
+        "StringLiteral<string *= /urandom/>"
       end
     end
   end
