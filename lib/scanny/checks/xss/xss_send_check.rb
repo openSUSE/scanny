55a9273ce4534562cec0f9a825492567b8918335
Piotr Niełacny <piotr.nielacny@gmail.com>
2012-08-03 12:10:47 +0200

    Add implementation for param argument
    Update CWE and impacts

--- lib/scanny/checks/xss/xss_send_check.rb
+++ lib/scanny/checks/xss/xss_send_check.rb
@@ -4,17 +4,25 @@ module Scanny
     # This can lead to download of private files from a server or to a XSS issue.
     class XssSendCheck < Check
       def pattern
-        pattern_send
+        [
+          pattern_send,
+          pattern_send_with_param
+        ].join("|")
       end
 
       def check(node)
-        issue :high, warning_message, :cwe => 79
+        if Machete.matches?(node, pattern_send)
+          issue :medium, warning_message, :cwe => [79, 115, 200]
+        elsif Machete.matches?(node, pattern_send_with_param)
+          issue :high, warning_message, :cwe => 201
+        end
       end
 
       private
 
       def warning_message
-        "Send file or data to client in \"inline\" mode can lead to XSS issues."
+        "Send file or data to client in \"inline\" " +
+        "mode or with param can lead to XSS issues."
       end
 
       # send_file "file.txt", :disposition => "inline"
@@ -28,10 +36,30 @@ module Scanny
               any,
               HashLiteral<
                 array = [
+                  any{even},
                   SymbolLiteral<value   = :disposition>,
-                  StringLiteral<string  = "inline">
+                  StringLiteral<string  = "inline">,
+                  any{even}
                 ]
               >
+            ]
+          >
+        >
+        EOT
+      end
+
+      def pattern_send_with_param
+        <<-EOT
+        SendWithArguments<
+          name = :send_file | :send_data,
+          arguments = ActualArguments<
+            array = [
+              any*,
+              SendWithArguments<
+                name = :[],
+                receiver = Send<name = :params>
+              >,
+              any*
             ]
           >
         >
