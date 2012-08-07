ff4cb26d68a15ad6d666bc1235150222a3f53158
Piotr Niełacny <piotr.nielacny@gmail.com>
2012-08-07 13:40:04 +0200

    Move specs from InputFiltering to XssLogger check

--- spec/scanny/checks/xss/xss_logger_check_spec.rb
+++ spec/scanny/checks/xss/xss_logger_check_spec.rb
@@ -5,7 +5,11 @@ module Scanny::Checks
     before :each do
       @runner = Scanny::Runner.new(XssLoggerCheck.new)
       @warning_message = "Assigning request parameters into logger can lead to XSS issues."
-      @issue  = Scanny::Issue.new("scanned_file.rb", 1, :low, @warning_message, 79)
+      @issue  = Scanny::Issue.new("scanned_file.rb", 1, :low, @warning_message, [20, 79])
+    end
+
+    it "reports \"logger(\"User \#{params[:password]} log\") correctly" do
+      @runner.should check('logger("User #{params[:password]} log")').with_issues(@issue)
     end
 
     it "reports \"logger(params[:password])\" correctly" do
