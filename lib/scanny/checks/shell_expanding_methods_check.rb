396083f9b1d055df3f04184aebdaed393eeb0165
Piotr Niełacny <piotr.nielacny@gmail.com>
2012-08-08 10:48:00 +0200

    Unitfy ShellExpandingMethodsCheck
    Merge ShellExpandingMethodsCheck with SystemMethodCheck

--- lib/scanny/checks/shell_expanding_methods_check.rb
+++ lib/scanny/checks/shell_expanding_methods_check.rb
@@ -4,8 +4,29 @@ module Scanny
     # through shell expansion. This can cause unwanted code execution if the
     # command includes unescaped input.
     class ShellExpandingMethodsCheck < Check
-      # system("command")
       def pattern
+        [
+          pattern_shell_expanding,
+          pattern_shell_execute,
+          pattern_popen,
+          pattern_execute_string
+        ].join("|")
+      end
+
+      def check(node)
+        if Machete.matches?(node, pattern_shell_expanding)
+          # The command goes through shell expansion only if it is passed as one
+          # argument.
+          message = "The \"#{node.name}\" method passes the executed command through shell expansion."
+        else
+          message = "Execute system commands can lead the system to run dangerous code"
+        end
+
+        issue :high, message, :cwe => [88, 78]
+      end
+
+      # system("rm -rf /")
+      def pattern_shell_expanding
         <<-EOT
           SendWithArguments<
             receiver  = Self | ConstantAccess<name = :Kernel>,
@@ -15,11 +36,20 @@ module Scanny
         EOT
       end
 
-      def check(node)
-        # The command goes through shell expansion only if it is passed as one
-        # argument.
-        issue :high, "The \"#{node.name}\" method passes the executed command through shell expansion.",
-              :cwe => [88, 78]
+      # Kernel.spawn("ls -lA")
+      def pattern_shell_execute
+        "SendWithArguments<name = :system | :spawn | :exec>"
+      end
+
+      # IO.popen
+      # IO.popen3
+      def pattern_popen
+        "SendWithArguments<name ^= :popen>"
+      end
+
+      # `system_command`
+      def pattern_execute_string
+        "ExecuteString"
       end
     end
   end
